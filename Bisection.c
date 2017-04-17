6783b6e57e370173b3f685179db350f9f7dd8a59
Kamrul Hasan Tusher <kmtusher97@gmail.com>
2017-04-17 23:04:10 +0600

    Update Bisection.c

--- Bisection.c
+++ Bisection.c
@@ -1,44 +1,32 @@
-/*
-        Bisection Method
-*/
+/* Bisection Method */
 #include <stdio.h>
-#define EPS 1e-7
 
-double function( double x ) { /* equation */
-        return ( x*x + x - 2 );
+float f( float x ) { 
+        return ( x*x - 4*x - 10 );
 }
 
-void Bisection( double x1, double x2 ) {
-        double x;
+void Bisection( float x1, float x2 ) {
+        float x;
         int i;
-        for(i=1; i<100; i++) {
+        for(i=1; i<200; i++) {
                 x = (x1+x2)/2.0;
-                printf("After iteration %3d Root = %.6lf\n", i, x);
-                if( function( x ) * function( x2 ) < 0 ) {
-                        x1 = x;
-                }
-                else {
-                        x2 = x;
-                }
-                if( fabs( function(x) ) < EPS ) {
-                        printf("\nRoot = %.6lf\n", x);
-                        printf("For x = %.6lf f(x) = %.6lf\n", x, function( x ));
-                        break;
-                }
+                if( f(x1) * f(x) < 0 ) x2 = x;
+                else x1 = x;
         }
+        printf("\nRoot = %.6lf\n", x);
+        printf("For x = %.6lf f(x) = %.6lf\n", x, f( x ));
 }
 
 int main() {
         printf("\nBisection Method\n\n");
         printf("Enter the starting values\n");
-        double x1, x2;
-        scanf("%lf %lf", &x1, &x2);
-        if( function( x1 ) * function( x2 ) >= 0.0 ) {
+        float x1, x2;
+        scanf("%f %f", &x1, &x2);
+        if( f( x1 ) * f( x2 ) >= 0.0 ) {
                 printf("\nThere is no root in this interval\n");
         }
         else {
                 Bisection( x1, x2 );
         }
-
         return 0;
 }
